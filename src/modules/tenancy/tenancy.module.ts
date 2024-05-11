eaa0072e735267e49a01a29f967ae1742ca8826b
Moaz Gabat <moaz.galbat@gmail.com>
2024-05-11 22:37:22 +0300

    fix bug in reading tenantId from request

--- src/modules/tenancy/tenancy.module.ts
+++ src/modules/tenancy/tenancy.module.ts
@@ -1,14 +1,13 @@
 import { Global, Module, Scope } from '@nestjs/common';
 import { CONNECTION } from './tenancy.symbols';
-import { Request as ExpressRequest } from 'express';
 import { getTenantConnection } from './tenancy.utils';
 import { REQUEST } from '@nestjs/core';
 
 const connectionFactory = {
   provide: CONNECTION,
   scope: Scope.REQUEST,
-  useFactory: (request: ExpressRequest) => {
-    const { tenantId } = request;
+  useFactory: (request: any) => {
+    const { tenantId } = request.req;
 
     if (tenantId) {
       return getTenantConnection(tenantId);
