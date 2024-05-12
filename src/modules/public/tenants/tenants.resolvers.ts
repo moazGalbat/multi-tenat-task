a7925e43b74c0d89a11726e346199ff44a0034dd
Moaz Gabat <moaz.galbat@gmail.com>
2024-05-13 00:14:48 +0300

    fix issues wih jwtstratgey

--- src/modules/public/tenants/tenants.resolvers.ts
+++ src/modules/public/tenants/tenants.resolvers.ts
@@ -2,11 +2,13 @@ import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { TenantsService } from './tenants.service';
 import { CreateTenantDto } from './dto/create-tenant.dto';
 import { Tenant } from './tenant.entity';
+import { Authorize } from 'src/modules/tenanted/auth/guards/authorize.guard';
 
 @Resolver('Tenant')
 export class TenantsResolver {
   constructor(private readonly tenantsService: TenantsService) {}
 
+  @Authorize()
   @Mutation(() => Tenant)
   async createTenant(
     @Args('createTenantInput') createTenantDto: CreateTenantDto,
--- src/modules/tenanted/auth/strategies/jwt.strategy.ts
+++ src/modules/tenanted/auth/strategies/jwt.strategy.ts
@@ -4,16 +4,12 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { User } from '../../users/user.entity';
 import { JwtDto } from '../dto/jwt.dto';
-import { ContextIdFactory, ModuleRef } from '@nestjs/core';
 import { Request } from 'express';
-import { AuthService } from '../auth.service';
+import { getTenantConnection } from 'src/modules/tenancy/tenancy.utils';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(
-    private moduleRef: ModuleRef,
-    readonly configService: ConfigService,
-  ) {
+  constructor(readonly configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: configService.get('JWT_ACCESS_SECRET'),
@@ -23,10 +19,12 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(request: Request, payload: JwtDto): Promise<User> {
-    const contextId = ContextIdFactory.getByRequest(request);
-    // "AuthService" is a request-scoped provider
-    const authService = await this.moduleRef.resolve(AuthService, contextId);
-    const user = await authService.validateUser(payload.userId);
+    if (!request.tenantId) {
+      throw new UnauthorizedException();
+    }
+    const connection = await getTenantConnection(request.tenantId);
+    const userRepo = connection.getRepository(User);
+    const user = await userRepo.findOne(payload.userId);
     if (!user) {
       throw new UnauthorizedException();
     }
