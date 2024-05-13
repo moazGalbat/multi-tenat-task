4b96024fdcf5f1e555964551917e198504a8dc0c
Moaz Gabat <moaz.galbat@gmail.com>
2024-05-13 10:22:44 +0300

    encypt user password before creatin by admin

--- src/modules/tenanted/users/users.module.ts
+++ src/modules/tenanted/users/users.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UserService } from './users.service';
 import { UserResolver } from './users.resolvers';
+import { PasswordService } from '../auth/password.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [UserService, UserResolver],
+  providers: [UserService, UserResolver, PasswordService],
   controllers: [],
 })
 export class UsersModule {}
--- src/modules/tenanted/users/users.service.ts
+++ src/modules/tenanted/users/users.service.ts
@@ -3,16 +3,23 @@ import { Connection, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { CONNECTION } from 'src/modules/tenancy/tenancy.symbols';
+import { PasswordService } from '../auth/password.service';
 
 @Injectable()
 export class UserService {
   private readonly userRepository: Repository<User>;
 
-  constructor(@Inject(CONNECTION) connection: Connection) {
+  constructor(
+    @Inject(CONNECTION) connection: Connection,
+    private passwordService: PasswordService,
+  ) {
     this.userRepository = connection.getRepository(User);
   }
 
   async createUser(createUserDto: CreateUserDto): Promise<User> {
+    createUserDto.password = await this.passwordService.hashPassword(
+      createUserDto.password,
+    );
     const user = this.userRepository.create(createUserDto);
     return this.userRepository.save(user);
   }
