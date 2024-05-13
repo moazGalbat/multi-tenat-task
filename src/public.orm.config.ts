d8c85d5123fd375b7ec54116c327fca707731118
Moaz Gabat <moaz.galbat@gmail.com>
2024-05-13 11:33:36 +0300

    add dockerfiler and docker-compose

--- src/public.orm.config.ts
+++ src/public.orm.config.ts
@@ -3,10 +3,10 @@ import { join } from 'path';
 
 module.exports = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
+  host: process.env.DB_HOST || 'postgres',
+  port: +process.env.DB_PORT || 5432,
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
   database: 'multi-tenant',
   logging: true,
   autoLoadEntities: true,
